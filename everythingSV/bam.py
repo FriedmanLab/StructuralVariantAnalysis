0698162a5ea794dcfa868a39bb48fde95ccc28e1
Madeline Couse <mcouse@cedar1.cedar.computecanada.ca>
2018-11-20 13:48:51 -0800

    add workdir to output path

--- everythingSV/bam.py
+++ everythingSV/bam.py
@@ -31,20 +31,16 @@ class bam_file():
 		self.reference = reference
 		self.samtools_path = samtools_path
 
-	def extract_discordants(self, output_prefix):
+	def extract_discordants(self, workdir, output_prefix):
 		"""
-		Runs LUMPY on a bwa-mem aligned whole genome bam to detect structural
-		variants.
+		Extracts discordant reads from bwa-mem aligned whole genome bam.
 
 		Parameters
 		----------
 
-		reference : str
-			path to reference genome
-		lumpy_dir : str
-			path to LUMPY directory
-		samtools_path : str
-			path to samtools executable
+		workdir: str 
+			path to working directory
+
 		output_prefix: str
 			path and prefix for output files
 
@@ -61,12 +57,12 @@ class bam_file():
 
 		"""
 		command_line = "samtools view -b -F 1294 {} > {}.discordants.unsorted.bam".format(
-				self.file_path, output_prefix)
+				self.file_path, workdir + "/" + output_prefix)
 		subprocess.call(command_line, shell=True)
 		return output_prefix
 
 
-	def extract_splitters(self, lumpy_dir, output_prefix):
+	def extract_splitters(self, lumpy_dir, workdir, output_prefix):
 		"""
 		Extracts split reads from bwa-mem aligned whole genome bam.
 
@@ -75,10 +71,10 @@ class bam_file():
 
 		lumpy_dir : str
 				path to LUMPY directory
-		samtools_path : str
-				path to samtools executable
-		working_dir : str
+
+		workdir : str
 				path to working directory
+
 		output_prefix: str
 				path and prefix for output files
 
@@ -94,11 +90,11 @@ class bam_file():
 
 		"""
 		command_line = "samtools view -h  {} | {}/scripts/extractSplitReads_BwaMem -i stdin | samtools view -Sb - >  {}.splitters.unsorted.bam".format(
-				self.file_path, lumpy_dir, output_prefix)
+				self.file_path, lumpy_dir, workdir + "/" + output_prefix)
 		subprocess.call(command_line, shell=True)
-		return output_prefix
+		return workdir + "/" + output_prefix
 
-	def sort_bam(self, bam_path, output_prefix):
+	def sort_bam(self, bam_path, workdir, output_prefix):
 		"""
 		Sort bam file
 
@@ -107,6 +103,9 @@ class bam_file():
 
 		bam_path: str
 				path to bam file
+				
+		workdir : str
+				path to working directory
 
 		output_prefix: str
 				path and prefix for output files
@@ -123,13 +122,13 @@ class bam_file():
 
 		"""
 		command_line = "samtools sort {} -o {}.sorted.bam -@ 2".format(
-				bam_path, output_prefix)
+				bam_path, workdir + "/" + output_prefix)
 		subprocess.call(command_line, shell=True)
-		return output_prefix
+		return workdir + "/" + output_prefix
 
 
 
-	def get_insert_size(self, lumpy_dir, output_prefix):
+	def get_insert_size(self, lumpy_dir, workdir, output_prefix):
 		"""
 		Get read insert size
 
@@ -138,6 +137,9 @@ class bam_file():
 
 		lumpy_dir : str
 				path to lumpy directory
+
+		workdir : str
+				path to working directory
 
 		output_prefix: str
 				path and prefix for output files
@@ -157,9 +159,9 @@ class bam_file():
 
 		"""
 		command_line = "samtools view {} | tail -n+100000 | {}/scripts/pairend_distro.py -r 150 -X 4 -N 10000 -o {} > {}  ".format(
-				self.file_path, lumpy_dir, output_prefix + ".histo", output_prefix +  ".insertsize")
+				self.file_path, lumpy_dir, workdir + "/" + output_prefix + ".histo", workdir + "/" + output_prefix +  ".insertsize")
 		subprocess.call(command_line, shell=True)
-		insert_stats = output_prefix + ".insertsize"
+		insert_stats = workdir + "/" + output_prefix + ".insertsize"
 		insert_stats = pd.read_csv(insert_stats, sep='\t', names=["Mean", "SD"])
 		mean = float(insert_stats.iloc[0,0].split(":")[1])
 		sd = float(insert_stats.iloc[0,1].split(":")[1])
@@ -170,7 +172,7 @@ class bam_file():
 
 
 	def run_LUMPY(
-		self, lumpy, output_prefix, insert_stats, read_length, min_mapq, num_support):
+		self, lumpy, workdir, output_prefix, insert_stats, read_length, min_mapq, num_support):
 		"""
 		Run LUMPY on bwa-mem aligned bam file to detect SVs
 
@@ -179,6 +181,9 @@ class bam_file():
 
 		lumpy : str
 				path to lumpy executable
+
+		workdir : str
+				path to working directory
 
 		output_prefix: str
 				path and prefix for output files
@@ -211,23 +216,36 @@ class bam_file():
 		ins_sd = insert_stats[1]
 
 		command_line = "{} -mw {} -tt 0 -pe id:{},bam_file:{},histo_file:{},mean:{},stdev:{},read_length:{},min_non_overlap:150,discordant_z:4,back_distance:20,weight:1,min_mapping_threshold:{} -sr id:{},bam_file:{},back_distance:20,weight:1,min_mapping_threshold:{} > {}".format(
-			lumpy, num_support, output_prefix, output_prefix + ".discordants.sorted.bam", output_prefix + ".histo",
-		ins_mean, ins_sd, read_length,min_mapq,output_prefix,output_prefix + ".splitters.sorted.bam",
-		min_mapq,output_prefix + ".LUMPY.vcf")
+			lumpy, num_support, output_prefix, workdir + "/" + output_prefix + ".discordants.sorted.bam",  workdir + "/" + output_prefix + ".histo",
+		ins_mean, ins_sd, read_length,min_mapq,output_prefix, workdir + "/" + output_prefix + ".splitters.sorted.bam",
+		min_mapq, workdir + "/" + output_prefix + ".LUMPY.vcf")
 
 		subprocess.call(command_line, shell=True)
-		return output_prefix + ".LUMPY.vcf"
+		return  workdir + "/" + output_prefix + ".LUMPY.vcf"
 
 	def run_CNVnator(
-		self, chromosomes, output_prefix):
+		self, CNVnator, cnvnator2VCF, bcftools, chromosomes, workdir, output_prefix):
 		"""
 		Runs CNVnator on a bwa-mem aligned whole genome bam to detect CNVs.
 
 		Parameters
 		----------
 
+		CNVnator : str
+			CNVnator executable
+
+		cnvnator2VCF : str
+			Path to cnvnator2VCF.pl script
+
+		bcftools : str
+			Path to bcftools executable
+
 		chromosomes : str
 			path to directory with chromosome.fa files
+
+		workdir : str
+				path to working directory
+
 		output_prefix: str
 			path and prefix for output files
 
@@ -243,28 +261,33 @@ class bam_file():
 		To do!
 
 		"""
-		command_line = "cnvnator -root {} -genome {} -tree {}".format(
-			output_prefix + ".root", self.reference, self.file_path)
+		command_line = "{} -root {} -genome {} -tree {} -unique".format(
+			CNVnator, workdir + "/" + output_prefix + ".root", self.reference, self.file_path)
 		subprocess.call(command_line, shell=True)
-		command_line = "cnvnator -root {} -genome {} -his {} -d {}".format(
-			output_prefix + ".root", self.reference, 1000, chromosomes)
+		command_line = "{} -root {} -genome {} -his {} -d {}".format(
+			CNVnator, workdir + "/" + output_prefix + ".root", self.reference, 1000, chromosomes)
 		subprocess.call(command_line, shell=True)
-		command_line = "cnvnator -root {} -genome {} -stat {}".format(
-			output_prefix + ".root", self.reference, 1000)
+		command_line = "{} -root {} -genome {} -stat {}".format(
+			CNVnator, workdir + "/" + output_prefix + ".root", self.reference, 1000)
 		subprocess.call(command_line, shell=True)
-		command_line = "cnvnator -root {} -genome {} -partition {}".format(
-			output_prefix + ".root", self.reference, 1000)
+		command_line = "{} -root {} -genome {} -partition {}".format(
+			CNVnator, workdir + "/" + output_prefix + ".root", self.reference, 1000)
 		subprocess.call(command_line, shell=True)
-		command_line = "cnvnator -root {} -genome {} -call {} > {}".format(
-			output_prefix + ".root", self.reference, 1000, output_prefix + ".CNVcall.1000")
+		command_line = "{} -root {} -genome {} -call {} > {}".format(
+			CNVnator, workdir + "/" + output_prefix + ".root", self.reference, 1000, workdir + "/" + output_prefix + ".CNVcall.1000")
 		subprocess.call(command_line, shell=True)
-		command_line = "cnvnator2VCF.pl {} {} > {}".format (
-			output_prefix + ".CNVcall.1000", chromosomes, output_prefix + ".CNVcall.1000.vcf" )
+		#convert calls to vcf format
+		command_line = "{} {} {} > {}".format (
+			cnvnator2VCF, workdir + "/" + output_prefix + ".CNVcall.1000", chromosomes, workdir + "/" + output_prefix + ".CNVcall.1000.vcf" )
 		subprocess.call(command_line, shell=True)
-		return output_prefix + ".CNVcall.1000.vcf"
+		#filter out calls with q0 < 0.5 
+		command_line = "{} filter -e 'INFO/natorQ0 < 0.5' {} > {}".format(bcftools, workdir + "/" + output_prefix + ".CNVcall.1000.vcf", workdir + "/" + output_prefix + ".CNVcall.1000.filter.vcf")
+		subprocess.call(command_line, shell=True)
+
+		return workdir + "/" + output_prefix + ".CNVcall.1000.filter.vcf"
 
 	def Manta_config(
-		self, manta_config, output_prefix):
+		self, manta_config, workdir, output_prefix):
 		"""
 		Generates Manta config
 
@@ -273,6 +296,9 @@ class bam_file():
 
 		manta_config: str
 			path to configManta.py (i.e. {MANTA_INSTALL_PATH}/bin/configManta.py)
+
+		workdir : str
+				path to working directory
 
 		output_prefix: str
 			path and prefix for output files
@@ -291,9 +317,9 @@ class bam_file():
 		"""
 
 		command_line = "{} --bam {} --reference {} --runDir {}_Manta".format(
-		manta_config, self.file_path, self.reference, output_prefix)
+		manta_config, self.file_path, self.reference, workdir + '/' + output_prefix)
 		subprocess.call(command_line, shell=True)
-		return output_prefix + '_Manta/'
+		return workdir + '/' + output_prefix + '_Manta/'
 
 
 	def run_Manta(
@@ -329,7 +355,7 @@ class bam_file():
 
 		return Manta_dir + "/results/variants/diploidSV.vcf"
 
-	def run_ERDS(self, ERDS_path, output_prefix, small_variant_vcf):
+	def run_ERDS(self, ERDS_path, workdir, output_prefix, small_variant_vcf):
 		"""
 		Runs ERDS on a bwa-mem aligned whole genome bam to detect CNVs.
 
@@ -337,6 +363,9 @@ class bam_file():
 		----------
 		ERDS_path : str
 			path to erds_pipeline.pl
+
+		workdir : str
+				path to working directory
 
 		output_prefix: str
 			path and prefix for output files
@@ -354,20 +383,23 @@ class bam_file():
 
 		"""
 		command_line = "{} -b {} -v {} -o {} -r {} ".format(
-				ERDS_path, self.file_path, small_variant_vcf, output_prefix + ".ERDS", self.reference)
+				ERDS_path, self.file_path, small_variant_vcf, workdir + '/' + output_prefix + ".ERDS", self.reference)
 		subprocess.call(command_line, shell=True)
-		return output_prefix + '.ERDS/'
+		return workdir + "/" + output_prefix + '.ERDS/' + output_prefix + '.erds.vcf'
 
 	def make_variant_file(
-		self, output_prefix, *args):
+		self, workdir, output_prefix, *args):
 		"""
 		Generates list of paths to vcf files generated by SV callers
 
 		Parameters
 		----------
 
+		workdir : str
+				path to working directory
+
 		output_prefix: str
-			path to output directory including sample name
+			sample name
 
 		*args : str
 			paths to SV vcf files
@@ -384,21 +416,25 @@ class bam_file():
 		To do!
 
 		"""
-		variant_list = open(output_prefix + "_variant_list.txt", 'w')
+		variant_list = open(workdir + '/' + output_prefix + "_variant_list.txt", 'w')
 
 		vcfs = ""
 
 		for arg in args:
-			vcfs = vcfs + arg + '\n'
+			if isinstance(arg, list):
+				for a in arg:
+					vcfs = vcfs + a + '\n'
+			else:
+				vcfs = vcfs + arg + '\n'
 
 		variant_list.write(vcfs)
 		variant_list.close()
 
-		return output_prefix + "_variant_list.txt"
+		return workdir + "/" +  output_prefix + "_variant_list.txt"
 
 
 	def run_SURVIVOR(
-		self,SURVIVOR_path, variant_list, max_dist, min_support, type, strand, estimate_distance, min_SV, output_prefix):
+		self,SURVIVOR_path, variant_list, max_dist, min_support, type, strand, estimate_distance, min_SV, workdir, output_prefix):
 		"""
 		Generates list of paths to vcf files generated by SV callers
 
@@ -428,8 +464,11 @@ class bam_file():
 		min_SV: int
 			minimum size of SVs to be taken into account
 
+		workdir : str
+				path to working directory
+
 		output_prefix: str
-			path to output directory including sample name
+			sample name
 
 
 		Returns
@@ -445,6 +484,6 @@ class bam_file():
 
 		"""
 		command_line = "{} merge {} {} {} {} {} {} {} {}".format(SURVIVOR_path, variant_list,
-max_dist, min_support, type, strand, estimate_distance, min_support, output_prefix + ".SURVIVOR.SV.vcf")
+max_dist, min_support, type, strand, estimate_distance, min_support, workdir + '/' + output_prefix + ".SURVIVOR.SV.vcf")
 		subprocess.call(command_line, shell=True)
-		return output_prefix + ".SURVIVOR.SV.vcf"
+		return workdir + '/' +  output_prefix + ".SURVIVOR.SV.vcf"
